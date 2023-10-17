0fb0e340fb21c41948966b95a1c9636432b199d8
Heazel_nuts <115053723+LauRizzo20@users.noreply.github.com>
2023-10-17 20:48:48 -0300

    eliminacion de listas añadida

--- eliminar_cde.php
+++ eliminar_cde.php
@@ -7,5 +7,12 @@ if (!isset($_POST['id'])) {
 
 $id_cde = $_POST['id'];
 
+$query = "DELETE FROM `tbl_listas` WHERE id = '$id_cde'";
+
+if ($sql = mysqli_query($conn, $query)) {
+    echo 'Lista eliminada';
+} else {
+    echo 'Vuelva a intentar';
+};
 
 ?>
