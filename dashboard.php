18436e9dba624d3235596c96e97c6f5db5fa47bd
NAWUI <nahuelgimenez303@gmai.com>
2023-10-18 17:13:13 -0300

    hola

--- dashboard.php
+++ dashboard.php
@@ -13,7 +13,6 @@
     <!-- <link rel="stylesheet" href="css/style_login.css"> -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
 </head>
 <body>
            <!-- HEADER INICIO -->
@@ -170,7 +169,9 @@
             <!-- Copyright -->
         </footer>
     </div>
+
 </body>
+
 <script src="js/sweetalert.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
 <script src="js/script.js"></script>
