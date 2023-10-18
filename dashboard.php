76c5b714099961ffdb4dc0923112d5c4d22370a1
NAWUI <nahuelgimenez303@gmai.com>
2023-10-18 13:39:27 -0300

    da

--- dashboard.php
+++ dashboard.php
@@ -173,7 +173,6 @@
 </body>
 <script src="js/sweetalert.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 <script src="js/script.js"></script>
 <script src="js\carga_lista.js"></script>
 
