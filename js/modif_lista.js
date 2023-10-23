3f9c71c869efb8060e07eeb943d22fec479bcd70
Heazel_nuts <115053723+LauRizzo20@users.noreply.github.com>
2023-10-23 17:33:33 -0300

    update:(dni no repetibles en modificacion lista)

--- js/modif_lista.js
+++ js/modif_lista.js
@@ -1,8 +1,33 @@
-$(document).ready(function(){
+$(document).ready(function() {
     $("#enviarFormulario").click(function() {
         // Obtener los datos del formulario
-        var formData = $("#formularioElecciones").serialize();
-        //alert(formData);
+        var formData = $("#formularioElecciones").serializeArray();
+        
+        // Crear un conjunto para almacenar valores únicos
+        var uniqueValues = new Set();
+        var hasDuplicates = false;
+
+        // Iterar sobre los datos del formulario
+        formData.forEach(function(input) {
+            if (uniqueValues.has(input.value)) {
+                hasDuplicates = true;
+                return false; // Detener la iteración si hay duplicados
+            } else {
+                uniqueValues.add(input.value);
+            }
+        });
+
+        if (hasDuplicates) {
+            swal({
+                icon: "error",
+                text: "Los valores en el formulario no deben repetirse.",
+                timer: 1500,
+                button: false
+            });
+            return; // No envía el formulario si hay duplicados
+        }
+
+        // Si no hay duplicados, procede a enviar el formulario
         $.ajax({
             method: "POST",
             url: "action_modif_lista.php",
@@ -13,7 +38,7 @@ $(document).ready(function(){
                 if (data == "Exito") {
                     swal({
                         icon: "success",
-                        title: "Lista modificada exitosamenmte",
+                        title: "Lista modificada exitosamente",
                         timer: 1100,
                         button: false
                     });
@@ -25,14 +50,14 @@ $(document).ready(function(){
                         timer: 1500,
                         button: false
                     });
-                } else if (data == "Se ha repetido un DNI de otra lista.") {
+                } else if (data == "DNI o nombre de lista repetidos.") {
                     swal({
                         icon: "error",
-                        text: "Se ha repetido un DNI de otra lista.",
+                        text: "DNI o nombre de lista repetidos.",
                         timer: 1500,
                         button: false
                     });
-                }else{
+                } else {
                     swal({
                         icon: "error",
                         text: "Error al enviar el formulario",
